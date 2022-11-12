087f7a4955f308ccc844e4fbc1ca7d3b9cc2ddbc
Erkmen Esen <erkmenesen@gmail.com>
2022-11-12 07:32:01 +0300

    I've added the .NET 6 features to .NET 5 version too.

---
+++ src/DotNetEd.CoreAdmin.DemoApp/Models/TestEntityWithImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetEd.CoreAdmin.DemoAppDotNet6.Models
+{
+    public class TestEntityWithImage
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public byte[]? Image { get; set; }
+    }
+}
